9cabf6bd0980b10f362e04cc57f2585f9633c507
Finbar Thomas <c3330602@uon.edu.au>
2019-04-11 07:39:00 +0000

    Added option for choosing encryption or decryption using switch

--- main.c
+++ main.c
@@ -1,14 +1,32 @@
  #include <stdio.h>
 
-int cypher;
+int cypher, cryption;
 
 int main() {
     printf("Input 1 for Rotaion or 2 for Substitution\n");
     scanf("%d", &cypher);
     switch(cypher) { //switch between cypher used
         case 1: printf("Rotation chosen\n");
+                printf("Input 1 for Encryption or 2 for Decryption\n");
+                scanf("%d", &cryption);
+                switch(cryption) { // switch between decryption and encryption
+                    case 1: printf("Encryption chosen");
+                        break;
+                    case 2: printf("Decryption chosen");
+                        break;
+                    default: printf("error");
+                }
             break;
         case 2: printf("Substitution chosen\n");
+                printf("Input 1 for Encryption or 2 for Decryption\n");
+                scanf("%d", &cryption);
+                switch(cryption) { // switch between decryption and encryption
+                    case 1: printf("Encryption chosen");
+                        break;
+                    case 2: printf("Decryption chosen");
+                        break;
+                    default: printf("error");
+                }
             break;
         default: printf("error");
     }
