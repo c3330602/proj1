4a885217c23028db53f19716c436450bddc88611
Finbar Thomas <c3330602@uon.edu.au>
2019-04-28 07:01:53 +0000

    Added rotation encryption

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
  #include <stdio.h>
+ #include <string.h>
+
+void rotatione(void);
 
 int cypher, cryption;
 
@@ -10,9 +13,11 @@ int main() {
                 printf("Input 1 for Encryption or 2 for Decryption\n");
                 scanf("%d", &cryption);
                 switch(cryption) { // switch between decryption and encryption
-                    case 1: printf("Encryption chosen");
+                    case 1: printf("Encryption chosen\n");
+                            rotatione();
                         break;
-                    case 2: printf("Decryption chosen");
+                    case 2: printf("Decryption chosen\n");
+                            
                         break;
                     default: printf("error");
                 }
@@ -31,4 +36,32 @@ int main() {
         default: printf("error");
     }
     return 0;
+}
+
+void rotatione(void) {
+    int key, difference, N = 0;
+    int length = 200; //change if not long enough
+    char message[200];
+    printf("choose key\n");
+    scanf("%d\n", &key);
+    printf("%d chosen as key\n", key);
+    scanf("%[^\n]s\n", message);
+    printf("message chosen to encrypt: %s\n", message);
+    
+    while(N < length) {
+        if(message[N] >= 65 && message[N] <= 90) { //only includes capital letters
+            
+            message[N] = message[N] + key; // adds or subtracts based off key
+            if(message[N] > 90) { //for when key goes past Z
+                difference = message[N] - 90;
+                message[N] = 64 + difference;
+            }if(message[N] < 65){ //for when key goes past A
+                difference = 65 - message[N];
+                message[N] = 91 - difference;
+            }
+        }
+        
+        N = N + 1;
+    }
+    printf("%s", message);
 }
